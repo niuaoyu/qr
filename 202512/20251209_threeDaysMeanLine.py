27da82477041c9e399331a7c6471c9c4919cccfb
KRIPB <2734849800@qq.com>
2025-12-11 21:18:31 +0800

    20251209

--- 202512/20251209_threeDaysMeanLine.py
+++ 202512/20251209_threeDaysMeanLine.py
@@ -1,8 +1,8 @@
 '''
 Strategy:You are a slightly less stupid stockholder
 You calculate the average price of the last 3 days
-    If today's price > SMA → BUY (trend up)
-    if today's price < SMA → do not buy (trend down)
+    If yesterday's price > SMA → today buy (trend up)
+    if yesterday's price < SMA → today do not buy (trend down)
 A little smarter than the last strategy: don't look at single days up or down, look at the trend.
 '''
 import numpy as np
@@ -15,27 +15,14 @@ df = pd.DataFrame({
     'day':day,
     'price':price
 })
-df['incOrNot'] = df['price'].pct_change().round(2)
+df['incOrNot'] = df['price'].pct_change()
 
-df['threeDaysMean'] = df['price'].rolling(window=3).mean().round(2)
-df['signalBuyOrNot'] = np.where(df['threeDaysMean'].shift(1)<df['price'],1,0)
+df['threeDaysMean'] = df['price'].rolling(window=3).mean()
 
-df['threeDaysMeanGains'] = (df['incOrNot'] *df['signalBuyOrNot']).cumsum().round(2)
-df['brainlessFixedInvestment'] = df['incOrNot'].cumsum().round(2)
+# understand why the first is wrong?
+# df['signalBuyOrNot'] = np.where(df['threeDaysMean'].shift(1)<df['price'],1,0)
+df['signalBuyOrNot'] = np.where((df['threeDaysMean']<df['price']).shift(1),1,0)
+
+df['threeDaysMeanGains'] = (1+df['incOrNot'] *df['signalBuyOrNot']).cumprod()-1
+df['brainlessFixedInvestment'] = (1+df['incOrNot']).cumprod()-1
 print(df)
-
-'''
-    day  price  incOrNot  threeDaysMean  signalBuyOrNot  threeDaysMeanGains  brainlessFixedInvestment
-0     1    100       NaN            NaN               0                 NaN                       NaN
-1     2    102      0.02            NaN               0                0.00                      0.02
-2     3     99     -0.03         100.33               0                0.00                     -0.01
-3     4     97     -0.02          99.33               0                0.00                     -0.03
-4     5    101      0.04          99.00               1                0.04                      0.01
-5     6    105      0.04         101.00               1                0.08                      0.05
-6     7    103     -0.02         103.00               1                0.06                      0.03
-7     8    108      0.05         105.33               1                0.11                      0.08
-8     9    106     -0.02         105.67               1                0.09                      0.06
-9    10    110      0.04         108.00               1                0.13                      0.10
-10   11    115      0.05         110.33               1                0.18                      0.15
-11   12    112     -0.03         112.33               1                0.15                      0.12
-'''
